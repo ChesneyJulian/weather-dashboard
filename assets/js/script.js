32fda253b21a9e15d27380dfed250400e05577f8
Chesney Julian <chesney.julian724@gmail.com>
2023-05-22 17:36:14 -0400

    begin creating fetch for weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+var city = "Knoxville"
+
+function getApi() {
+   
+    var apiKey = "270870f74b0a4167c2dabb9a30b68d16" 
+    var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+
+    fetch(requestUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+
+}
+
+getApi();
