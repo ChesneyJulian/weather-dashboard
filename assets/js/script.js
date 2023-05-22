098a41853343a4ddc97c54119308d166dff85d6c
Chesney Julian <chesney.julian724@gmail.com>
2023-05-22 19:08:28 -0400

    edit js and html so textbox input is used for getApi and stored to local storage when searchbtn is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,28 @@
-var city = "Knoxville"
 
-function getApi() {
-   
+
+function getApi(city) {
+    
     var apiKey = "270870f74b0a4167c2dabb9a30b68d16" 
-    var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+    var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + "&units=imperial";
 
     fetch(requestUrl)
     .then(function(response) {
         return response.json();
     })
     .then(function(data){
+        var coord = data.coord; 
         console.log(data);
+        console.log(coord);
+        
     })
 
 }
 
-getApi();
+document.getElementById('search-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    var input = document.getElementById('TextInput').value;
+    localStorage.setItem('city', JSON.stringify(input));
+    var city = JSON.parse(localStorage.getItem('city'))
+    console.log(city); 
+    getApi(city);
+})
