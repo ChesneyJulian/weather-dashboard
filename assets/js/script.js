06a6af0e15aee60f119b011c5efe34d8b44adf46
Chesney Julian <chesney.julian724@gmail.com>
2023-05-22 19:35:56 -0400

    edit eventlistener so all cities are saved to array and in local storage while calling getApi with the most recent searched city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 
-
+// create function to fetch openweathermap api 
 function getApi(city) {
-    
     var apiKey = "270870f74b0a4167c2dabb9a30b68d16" 
     var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + "&units=imperial";
 
@@ -10,6 +9,7 @@ function getApi(city) {
         return response.json();
     })
     .then(function(data){
+        // set data.coord to var coord so the city's coordinates are saved 
         var coord = data.coord; 
         console.log(data);
         console.log(coord);
@@ -17,12 +17,22 @@ function getApi(city) {
     })
 
 }
-
+// set empty array to store all searched cities
+var cities = []
 document.getElementById('search-btn').addEventListener('click', function(event){
     event.preventDefault();
+    // set textinput value to var input using DOM
     var input = document.getElementById('TextInput').value;
-    localStorage.setItem('city', JSON.stringify(input));
-    var city = JSON.parse(localStorage.getItem('city'))
-    console.log(city); 
-    getApi(city);
+    // push input to cities array 
+    cities.push(input);
+    // use cities array to store searched cities in local storage with key 'city'
+    localStorage.setItem('city', JSON.stringify(cities));
+    // set var city to value of localstorage('city')
+    var city = JSON.parse(localStorage.getItem('city'));
+    // set var currentCity to last item in city array because it is the last city entered and the current one being searched
+    var currentCity = city[city.length - 1];
+    console.log(currentCity); 
+    // call getApi function using currentcity
+    getApi(currentCity);
+    
 })
