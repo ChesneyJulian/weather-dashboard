03e6c86963c3962d68c010351f3bba98d5d06dfb
Chesney Julian <chesney.julian724@gmail.com>
2023-05-22 21:14:56 -0400

    edit getApi function to display desired data onto main card element in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-
-// create function to fetch openweathermap api 
-function getApi(city) {
     var apiKey = "270870f74b0a4167c2dabb9a30b68d16" 
+// create function to fetch openweathermap api 
+var getApi = function (city) {
+
     var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + "&units=imperial";
 
     fetch(requestUrl)
@@ -11,12 +11,30 @@ function getApi(city) {
     .then(function(data){
         // set data.coord to var coord so the city's coordinates are saved 
         var coord = data.coord; 
+        // set lon and lat to separate variables to be used with forecast api
+        var lon = coord.lon;
+        var lat = coord.lat;
         console.log(data);
-        console.log(coord);
+
+        getForecast(lat, lon);
         
+        // set textcontent for main card using data from api fetch
+        var tempEl = document.getElementById('temp');
+        var windEl = document.getElementById('wind');
+        var humidityEl = document.getElementById('humidity');
+        var mainCardHeader = document.getElementById('main-card-header');
+        tempEl.textContent = "Temp: " + data.main.temp;
+        windEl.textContent = "Wind: " + data.wind.speed;
+        humidityEl.textContent = "Humidity: " + data.main.humidity;
+        mainCardHeader.textContent = data.name;
     })
 
 }
+
+var getForecast = function (lat, lon) {
+    var apiUrl = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon="+ lon +"&appid=" + apiKey;
+}
+
 // set empty array to store all searched cities
 var cities = []
 document.getElementById('search-btn').addEventListener('click', function(event){
